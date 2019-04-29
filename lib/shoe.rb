5faa12fff8b5ebaefb562ee7f852947c91a16b51
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-29 04:44:38 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,9 +4,14 @@ class Shoe
 
   BRANDS = []
 
-  def initialize(brands)
+  def initialize(brand)
     @brands = brands
-    
+    def initialize(brand)
+    @brand = brand
+    if !(BRANDS.include?(@brand))
+      BRANDS << @brand
+    end
+  end
     
   end
 
